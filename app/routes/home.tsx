38ccc4693cd09eede3c59072781bee5653998e54
prrraveenkumar <pk248539@gmail.com>
2026-06-23 06:05:26 +0530

    feat: integrate Puter.js for enhanced resume analysis and feedback
    - Added Navbar component for navigation.
    - Introduced ResumeCard component to display individual resumes with feedback.
    - Implemented ScoreCircle component for visual score representation.
    - Created constants for sample resumes and AI response format.
    - Established Zustand store for managing Puter.js integration and state.
    - Updated CSS styles for improved layout and responsiveness.
    - Added background image to the home page.
    - Included script for Puter.js in the root layout.
    - Defined TypeScript interfaces for Job, Resume, Feedback, and Puter-related types.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import type { Route } from "./+types/home";
+import Navbar from '~/components/Navbar'
+import ResumeCard from '~/components/ResumeCard'
+import { resumes } from '~/constants/index'
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -8,7 +11,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1 >Track your Applications & Resume ratings</h1>
+        <h2>Review your submission and check AI powered feedback</h2>
+      </div>
+      
 
+      {resumes?.length > 0 && (
+        <div className="resume-section">
+          {resumes.map((resume) => (
+            <ResumeCard
+              key={resume.id}
+              resume={resume}
+            />
+          ))}
+        </div>
+      )}
+    </section>
   </main>;
 }
